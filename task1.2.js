2da7da0038ee507609eaeba450c52db65eac6318
Esha Gautam <eshagautam0@gmail.com>
2024-01-03 21:49:40 +0530

    task1.3 to task1.2

--- task1.2.js
+++ task1.2.js
@@ -1,4 +1,5 @@
-let fruits = document.getElementsByTagName('li');
+let fruits = document.git add -A
+git commit -m "Rename folder to 'new_folder_name'"('li');
 
 if (fruits.length >= 5) {
   fruits[4].style.color = 'blue';
